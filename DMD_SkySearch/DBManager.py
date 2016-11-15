51c34a67cbc51187d856628235696da3cf2b391c
TimKah <timkah@mail.ru>
2016-11-15 15:24:57 +0300

    missed

---
+++ DMD_SkySearch/DBManager.py
@@ -0,0 +1,12 @@
+﻿import psycopg2
+
+class DBManager:
+    def __init__(self, name):
+        try:
+            self.conn = psycopg2.connect("dbname=25655 user=postgres password=0000")
+        except psycopg2.Error as e:
+            print("I am unable to connect to the database")
+            print(e)
+            print(e.pgcode)
+            print(e.pgerror)
+            print(traceback.format_exc())
